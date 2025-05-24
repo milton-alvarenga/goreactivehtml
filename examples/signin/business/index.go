efb6dd77b4991fb18e7cf8ca7b0ccdf3e52428c5
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-05-24 00:58:33 -0300

    feat: Add error message on sign in example

--- examples/signin/business/index.go
+++ examples/signin/business/index.go
@@ -9,7 +9,12 @@ func main(){
 }
 
 func SignIn(Email string, Key string) bool{
+	ErrorMsg = ""
 	//Check if the login and password are ok
+
+	if err != nil {
+		ErrorMsg = "Wrong login and/or password"
+	}
 
 	return success
 }

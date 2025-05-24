05b770c005aa547f2eb31a4e6fcc59fb5cbdc635
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-05-24 00:51:42 -0300

    feat: Add signin example

---
+++ examples/signin/business/index.go
@@ -0,0 +1,15 @@
+package business
+
+var Email string
+var Key string
+var ErrorMsg string
+
+func main(){
+
+}
+
+func SignIn(Email string, Key string) bool{
+	//Check if the login and password are ok
+
+	return success
+}

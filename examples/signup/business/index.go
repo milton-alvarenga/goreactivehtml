76d2fe2dce9e589c9c018e01a9bfe00db05d0b1a
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-05-24 02:42:01 -0300

    feat: Add signup example

---
+++ examples/signup/business/index.go
@@ -0,0 +1,20 @@
+package business
+
+import(
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+var Email string
+var Password string
+var ConfirmPassword string
+
+func main (){
+
+}
+
+
+func SubmitSignup(Email string, Password string, ConfirmPassword string){
+
+}

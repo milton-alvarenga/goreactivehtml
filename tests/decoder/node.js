64e69e5e00889352f85ee4e6a8956c7ad4589a55
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-11-27 00:19:17 -0300

    test: Update decoder/node.js debug messages

--- tests/decoder/node.js
+++ tests/decoder/node.js
@@ -1,19 +1,25 @@
 import { applyBinaryOperation } from "./../../web/lib/ArrayDecodeProtocol.js";
 import fs from 'fs';
 
-process.stdout.write("Starting node\n");
+let debug = false
+
+if(debug){
+    process.stdout.write("Starting node\n");
+}
 
 (async () => {
     try {
         const buffer = fs.readFileSync(0); // Read from STDIN
-        console.log("Received input:", buffer.toString()); // Ensure the data is as expected
+        if(debug){
+            console.log("Received input:", buffer.toString()); // Ensure the data is as expected
+        }
 
         if (buffer.length === 0) {
             throw new Error("Received empty input.");
         }
 
         const target = [];
-        applyBinaryOperation(buffer, target, true);
+        applyBinaryOperation(buffer, target, debug);
         
         process.stdout.write(JSON.stringify(target) + "\n");
     } catch (err) {
